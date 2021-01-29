35565aa46b68694e16b64f53416e11ef06dd5869
Lynn1207 <linlintoo2@gmail.com>
2021-01-28 21:15:37 -0500

    Add files via upload

---
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -0,0 +1,299 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+import sys
+import tarfile
+import random
+
+from six.moves import urllib
+import tensorflow.compat.v1 as tf
+import numpy as np
+
+import cnnHAR_input
+
+# Basic model parameters.
+batch_size = 32
+                          
+data_dir = '/Users/lynn/Documents/CNN_Human_Activity_Recognition/data/'
+                    
+
+# Global constants describing the CIFAR-10 data set.
+SIGNAL_SIZE = cnnHAR_input.SIGNAL_SIZE
+NUM_CLASSES = cnnHAR_input.NUM_CLASSES
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cnnHAR_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cnnHAR_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+
+# Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0     # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.05      # Initial learning rate.
+
+# If a model is trained with multiple GPUs, prefix all Op names with tower_name
+# to differentiate the operations. Note that this prefix is removed from the
+# names of the summaries when visualizing a model.
+TOWER_NAME = 'tower'
+
+def distorted_inputs():
+  """Construct distorted input for CIFAR training using the Reader ops.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+
+  Raises:
+    ValueError: If no data_dir
+  """
+  global data_dir
+  data_dir = data_dir
+  signals, labels= cnnHAR_input.distorted_inputs(data_dir=data_dir,batch_size=batch_size)
+  signals = tf.cast(signals, tf.float64)
+  labels = tf.cast(labels, tf.float64)
+  return signals, labels
+  
+def inputs(eval_data):
+    global data_dir
+    data_dir = data_dir
+    signals, labels = cnnHAR_input.inputs(eval_data ,data_dir=data_dir,batch_size=batch_size)
+    signals = tf.cast(signals, tf.float64)
+    labels = tf.cast(labels, tf.float64)
+    return signals, labels
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  """Helper to create an initialized Variable with weight decay.
+
+  Note that the Variable is initialized with a truncated normal distribution.
+  A weight decay is added only if one is specified.
+  Args:
+    name: name of the variable
+    shape: list of ints
+    stddev: standard deviation of a truncated Gaussian
+    wd: add L2Loss weight decay multiplied by this float. If None, weight
+        decay is not added for this Variable.
+  Returns:
+    Variable Tensor
+  """
+  dtype = tf.float64
+  var = _variable_on_cpu(
+      name,
+      shape,
+      tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+  #i=var.op.name.find('/')-2
+  '''
+  if wd is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    tf.add_to_collection('losses', weight_decay)
+  '''
+  return var
+  
+def _variable_on_cpu(name, shape, initializer):
+  """Helper to create a Variable stored on CPU memory.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    initializer: initializer for Variable
+
+  Returns:
+    Variable Tensor
+  """
+  #with tf.device('/cpu:'+str(int(sys.argv[1])-1)):
+  dtype = tf.float64
+  var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+  return var
+  
+def _activation_summary(x):
+  """Helper to create summaries for activations.
+
+  Creates a summary that provides a histogram of activations.
+  Creates a summary that measures the sparsity of activations.
+
+  Args:
+    x: Tensor
+  Returns:
+    nothing
+  """
+  # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+  # session. This helps the clarity of presentation on tensorboard.
+  tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+  tf.summary.histogram(tensor_name + '/activations', x)
+  tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
+def _add_loss_summaries(total_loss):
+  """Add summaries for losses in CIFAR-10 model.
+
+  Generates moving average for all losses and associated summaries for
+  visualizing the performance of the network.
+
+  Args:
+    total_loss: Total loss from loss().
+  Returns:
+    loss_averages_op: op for generating moving averages of losses.
+  """
+  # Compute the moving average of all individual losses and the total loss.
+  loss_averages = tf.train.ExponentialMovingAverage(0.9999, name='avg')
+  losses = tf.get_collection('losses')
+  loss_averages_op = loss_averages.apply(losses + [total_loss])
+  tf.add_to_collection('losses', total_loss)
+
+  return loss_averages_op
+  
+def inference(signals):
+    #print ('<<<<<<<<<<<<<<<<<<<<Shape of signals :',signals.get_shape())
+    with tf.variable_scope('conv1') as scope:
+           kernel = _variable_with_weight_decay('weights',
+                                                shape=[ 32, 1, 64],
+                                                #shape=[3, 1, 128],
+                                                stddev=5e-2,
+                                                wd=0.001)
+           biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))#!!!
+           conv = tf.nn.conv1d(signals, kernel, [1,12,1], padding='VALID', data_format='NWC')
+           pre_activation = tf.nn.bias_add(conv, biases)
+           conv1 = tf.nn.relu(pre_activation, name=scope.name)
+           _activation_summary(conv1)
+           #print ('<<<<<<<<<<<<<<<<<<<<Shape of conv1 :',conv1.get_shape())
+    pool1 = tf.nn.max_pool1d(conv1, ksize=[1,4,1], strides=[1,1,1],padding='VALID',name='pool1')
+    #print ('<<<<<<<<<<<<<<<<<<<<Shape of pool1 :',pool1.get_shape())
+    """6x1x64"""
+    
+    with tf.variable_scope('conv2') as scope:
+           kernel = _variable_with_weight_decay('weights',
+                                                shape=[ 3, 64, 32],
+                                                #shape=[3, 1, 128],
+                                                stddev=5e-2,
+                                                wd=0.001)
+           biases = _variable_on_cpu('biases', [32], tf.constant_initializer(0.0))#!!!
+           conv = tf.nn.conv1d(pool1, kernel, [1,1,1], padding='VALID', data_format='NWC')
+           pre_activation = tf.nn.bias_add(conv, biases)
+           conv2 = tf.nn.relu(pre_activation, name=scope.name)
+           _activation_summary(conv2)
+           #print ('<<<<<<<<<<<<<<<<<<<<Shape of conv2:',conv2.get_shape())
+    pool2 = tf.nn.max_pool1d(conv2, ksize=[1,3,1], strides=[1,1,1],padding='VALID',name='pool2')
+    #print ('<<<<<<<<<<<<<<<<<<<<Shape of pool2 :',pool2.get_shape()) 
+    reshape = tf.keras.layers.Flatten()(pool2)
+    #print ('<<<<<<<<<<<<<<<<<<<<Shape of reshape :',reshape.get_shape()) 
+    reshape = tf.cast(reshape, tf.float64)
+    """2x32"""
+    
+    dim = reshape.get_shape()[1]
+     
+    with tf.variable_scope('local2') as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        weights = _variable_with_weight_decay('weights', shape=[dim, 1024],
+                                              stddev=0.04, wd=0.001)
+        biases = _variable_on_cpu('biases', [1024], tf.constant_initializer(0.10))
+        
+        local2 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        #print ('!!!!!!!!!!!!!!!Shape of local2 :', local2.get_shape())
+        _activation_summary(local2)
+
+    with tf.variable_scope('local3') as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        weights = _variable_with_weight_decay('weights', shape=[1024, 512],
+                                              stddev=0.04, wd=0.001)#0.004,index)
+        biases = _variable_on_cpu('biases', [512], tf.constant_initializer(0.10))
+        
+        local3 = tf.nn.relu(tf.matmul(local2, weights) + biases, name=scope.name)
+        #print ('!!!!!!!!!!!!!!!Shape of local3 :', local3.get_shape())
+        _activation_summary(local3)
+
+    with tf.variable_scope('local4') as scope:
+        weights = _variable_with_weight_decay('weights', shape=[512, 30], stddev=0.04, wd=0.001)
+        biases = _variable_on_cpu('biases', [30], tf.constant_initializer(0.10))
+            
+        local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+        #print ('!!!!!!!!!!!!!!!Shape of local4 :', local4.get_shape())#256
+        _activation_summary(local4)
+
+    with tf.variable_scope('softmax_linear') as scope:
+          weights = _variable_with_weight_decay('weights', [30, NUM_CLASSES],stddev=1/30.0, wd=0.001)
+          biases = _variable_on_cpu('biases', [NUM_CLASSES],tf.constant_initializer(0.0))
+          softmax_linear = tf.nn.softmax(tf.matmul(local4, weights)+biases,name=scope.name)
+          _activation_summary(softmax_linear)
+          #print ('!!!!!!!!!!!!!!!Shape of softmax_linear :', softmax_linear.get_shape())
+    
+    return softmax_linear
+    
+
+def loss(logits, labels):
+    labels = tf.cast(labels, tf.int64)
+    labels = tf.reshape(labels, [batch_size,1])
+    logits = tf.reshape(logits, [batch_size,1,NUM_CLASSES])
+    i=0
+    loss=0.0
+    
+    while i<batch_size:
+        loss+=-tf.math.log(logits[i,0,labels[i,0]])
+        i+=1
+    loss=loss/32.0
+    
+    #print('loss@@@@@@@@@@@@##############',loss)
+    '''
+    tf.add_to_collection('losses'+index, loss)
+    # The total loss is defined as the cross entropy loss plus all of the weight
+    # decay terms (L2 loss).
+    total_loss=tf.add_n(tf.get_collection('losses'+index),name='total_loss')
+    '''
+    return loss
+    
+    
+def train(total_loss, global_step):#index is a string e.g. '_1'
+ 
+ num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / batch_size
+ decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+ # Decay the learning rate exponentially based on the number of steps.
+ lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                global_step,
+                                decay_steps,#10000,
+                                LEARNING_RATE_DECAY_FACTOR,
+                                staircase=True)
+ tf.summary.scalar('learning_rate', lr)
+
+ """
+ var_list=[]
+ for var in tf.trainable_variables():
+     if var.op.name.find(index)!= -1:
+        var_list.append(var)
+        #print('@@@@@@@@@@@@@@@@@@'+var.op.name)
+        '''
+        if var.op.name.find('weights')!= -1 and var.op.name.find('conv')==-1 and var.op.name.find('soft')==-1:
+            weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+            total_loss+= weight_decay
+        '''
+ """
+        
+ # Generate moving averages of all losses and associated summaries.
+ loss_averages_op = _add_loss_summaries(total_loss)
+
+ # Compute gradients.
+ with tf.control_dependencies([loss_averages_op]):
+  opt = tf.train.MomentumOptimizer(lr, 0.5)#opt = tf.train.AdadeltaOptimizer(lr)
+  grads = opt.compute_gradients(total_loss)
+  #for i in range(0,len(grads)):
+    #print(i)
+    #print('<<<<<<<<<<<<<<<<< shape of grads:',grads[i])
+
+# Apply gradients.
+ apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+# Add histograms for trainable variables.
+ for var in tf.trainable_variables():
+  tf.summary.histogram(var.op.name, var)
+  #print(var.op.name)
+
+# Add histograms for gradients.
+ for grad, var in grads:
+  if grad is not None:
+    tf.summary.histogram(var.op.name + '/gradients', grad)
+
+# Track the moving averages of all trainable variables.
+ variable_averages = tf.train.ExponentialMovingAverage(
+    MOVING_AVERAGE_DECAY, global_step)
+ with tf.control_dependencies([apply_gradient_op]):
+  variables_averages_op = variable_averages.apply(tf.trainable_variables())
+  
+ return variables_averages_op
--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -1,1 +1,196 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A binary to train CIFAR-10 using a single GPU.
+
+Accuracy:
+cifar10_train.py achieves ~86% accuracy after 100K steps (256 epochs of
+data) as judged by cifar10_eval.py.
+
+Speed: With batch_size 128.
+
+System        | Step Time (sec/batch)  |     Accuracy
+------------------------------------------------------------------
+1 Tesla K20m  | 0.35-0.60              | ~86% at 60K steps  (5 hours)
+1 Tesla K40m  | 0.25-0.35              | ~86% at 100K steps (4 hours)
+
+Usage:
+Please see the tutorial and website for how to download the CIFAR-10
+data set, compile the program and train the model.
+
+http://tensorflow.org/tutorials/deep_cnn/
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import time
+import sys
+import tensorflow.compat.v1 as tf
+import numpy as np
+from tensorflow.python import debug as tfdbg 
+
+import cnnHAR
+import cnnHAR_eval
+
+train_dir = '/Users/lynn/Documents/CNN_Human_Activity_Recognition/cnnHAR_check0'+str(sys.argv[1])
+
+num=1 # number of nodes
+
+max_steps = 7*450+1 #400 epoch
+
+log_device_placement = False
+
+log_frequency = 7 # log per epoch
+
+batch_size = cnnHAR.batch_size
+
+NUM_CLASSES = cnnHAR.NUM_CLASSES
+def train():
+  """Train CIFAR-10 for a number of steps."""
+  with tf.Graph().as_default():
+    global_step = tf.train.get_or_create_global_step()
+    # Get images and labels for CIFAR-10.
+    # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
+    # GPU and resulting in a slow down.
+    #with tf.device('/cpu:'+str(int(sys.argv[1])-1)):
+    signals, labels = cnnHAR.distorted_inputs()
+    print('<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>')
+      
+    # Build a Graph that computes the logits predictions from the
+    # inference model.
+    #training = tf.placeholder(tf.bool)
+    
+    logits=cnnHAR.inference(signals)
+
+    loss=cnnHAR.loss(logits, labels)
+                                     
+    train_op = cnnHAR.train(loss, global_step)
+  
+    extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    
+    class _LoggerHook(tf.train.SessionRunHook):
+      """Logs loss and runtime."""
+
+      def begin(self):
+        self._step = -1
+        self._start_time = time.time()
+
+      def before_run(self, run_context):
+        self._step += 1
+        #print('~~~~~~~~~~~~~~~~before run1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        #tmp = tf.concat([labels,signals],1)
+        return tf.train.SessionRunArgs(loss)# Asks for loss value.
+
+      def after_run(self, run_context, run_values):
+#        if self._step == 1000:
+#          #tf.Session().run(tf.global_variables_initializer())
+#          ndar = np.array(run_values.results)
+#          np.savetxt("logits.csv", ndar.reshape(128,256), delimiter=",")
+        
+        if (self._step-1) % (log_frequency*10)==0:
+          #print('~~~~~~~~~~~~~~~~after run1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+          '''
+          current_time = time.time()
+          duration = current_time - self._start_time
+          self._start_time = current_time
+          
+          loss_value = run_values.results
+          examples_per_sec = log_frequency * batch_size / duration
+          sec_per_batch = float(duration / log_frequency)
+          format_str = ('%s: step %d, loss = %.8f (%.1f examples/sec; %.3f '
+                        'sec/batch)')
+          '''
+          format_str = ('User'+str(sys.argv[1])+': %s: step %d loss=%0.8f')
+          print(format_str % (datetime.now(), self._step, run_values.results))
+         
+    class _LoggerHook2(tf.train.SessionRunHook):
+      """Logs signals."""
+
+      def begin(self):
+        self._step = -1
+
+      def before_run(self, run_context):
+        self._step += 1
+        #print('~~~~~~~~~~~~~~~~before run2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        return tf.train.SessionRunArgs(logits)  # Asks for logits.
+
+      def after_run(self, run_context, run_values):
+        if self._step == max_steps-1:#:
+          print('~~~~~~~~~~~~~~~~after run2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+          ndar = np.array(run_values.results)
+          np.savetxt("logits"+str(self._step)+".csv", ndar.reshape(batch_size,NUM_CLASSES), delimiter=",")
+
+    class _LoggerHook3(tf.train.SessionRunHook):
+      """Logs labels."""
+
+      def begin(self):
+        self._step = -1
+
+      def before_run(self, run_context):
+        self._step += 1
+        return tf.train.SessionRunArgs(labels)  # Asks for labels.
+
+      def after_run(self, run_context, run_values):
+        if self._step == max_steps-1:
+          ndar = np.array(run_values.results)
+          np.savetxt("labels"+str(self._step)+".csv", ndar.reshape(batch_size,NUM_CLASSES), delimiter=",")
+
+    class _LoggerHook4(tf.train.SessionRunHook):
+      """Logs signals."""
+
+      def begin(self):
+        self._step = -1
+
+      def before_run(self, run_context):
+        self._step += 1
+        #print('~~~~~~~~~~~~~~~~before run4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        #return tf.train.SessionRunArgs(signals)  # Asks for signals.
+
+      def after_run(self, run_context, run_values):
+        if (self._step+1)% (50*log_frequency) == 0:
+        #if self._step == max_steps-1:#:
+          #print('~~~~~~~~~~~~~~~~after run4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+          cnnHAR_eval.main()
+
+    with tf.train.MonitoredTrainingSession(
+        checkpoint_dir=train_dir,
+        hooks=[tf.train.StopAtStepHook(last_step=max_steps),
+               #tf.train.NanTensorHook(loss),
+               _LoggerHook(),
+               #_LoggerHook2(),
+               _LoggerHook4()],#,save_checkpoint_steps=5000
+        config=tf.ConfigProto(
+            log_device_placement=log_device_placement),save_checkpoint_steps=50*log_frequency) as mon_sess:
+      ''',save_checkpoint_steps=10*log_frequency'''
+      
+      while not mon_sess.should_stop():
+        #print('~~~~~~~~~~~~~~~~%d step:'%i)
+        mon_sess.run([train_op,extra_update_ops])
+        #print('~~~~~~~~~~~~~~~~one session ends~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+def main(argv=None):  # pylint: disable=unused-argument
+#  cifar10.maybe_download_and_extract()
+  if tf.gfile.Exists(train_dir):
+    tf.gfile.DeleteRecursively(train_dir)
+  tf.gfile.MakeDirs(train_dir)
+  train()
+
+
+if __name__ == '__main__':
+  tf.app.run()
 
