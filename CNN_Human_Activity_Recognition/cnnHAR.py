d197421440d58efafd1f593dd81268c4aee0012d
Lynn1207 <linlintoo2@gmail.com>
2021-02-09 00:07:43 -0500

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -143,9 +143,9 @@ def _add_loss_summaries(total_loss):
   return loss_averages_op
   
 def inference(signals):
-    print("User"+str(sys.argv[1])+"'s trainable variables::::::::::::::::::::")
+    print("User"+str(sys.argv[1])+" trainable variables::::::::::::::::::::")
     for var in tf.trainable_variables():
-      print(var.op.name)
+      print("::::::::::::::::::::::::::::::::::::", var.op.name)
     #print ('<<<<<<<<<<<<<<<<<<<<Shape of signals :',signals.get_shape())
     with tf.variable_scope('conv1') as scope:
            kernel = _variable_with_weight_decay('weights',
@@ -163,6 +163,10 @@ def inference(signals):
     #print ('<<<<<<<<<<<<<<<<<<<<Shape of pool1 :',pool1.get_shape())
     """6x1x64"""
     
+    print("User"+str(sys.argv[1])+"trainable variables after cov1::::::::::::::::::::")
+    for var in tf.trainable_variables():
+      print("::::::::::::::::::::::::::::::::::::", var.op.name)
+      
     with tf.variable_scope('conv2') as scope:
            kernel = _variable_with_weight_decay('weights',
                                                 shape=[ 3, 64, 32],
