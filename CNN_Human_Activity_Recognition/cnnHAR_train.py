00f716d89b5ccf09a2852da50436c6a083bc309a
Lynn1207 <linlintoo2@gmail.com>
2021-02-08 20:04:13 -0500

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -47,7 +47,7 @@ from tensorflow.python import debug as tfdbg
 import cnnHAR
 import cnnHAR_eval
 
-train_dir = '/Users/lynn/Documents/CNN_Human_Activity_Recognition/cnnHAR_check0'+str(sys.argv[1])
+train_dir = '/home/ubuntu/deepHAR/CNN_Human_Activity_Recognition/cnnHAR_check0'+str(sys.argv[1])
 
 num=1 # number of nodes
 
