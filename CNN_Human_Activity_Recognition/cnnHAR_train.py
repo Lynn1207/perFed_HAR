96eb0d8a5373fef5864771a810f424d362d7a34a
Lynn1207 <linlintoo2@gmail.com>
2021-02-20 00:39:03 -0500

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -204,8 +204,8 @@ def train():
       
         #receive aggregated weights from server
         W_general = comm.recvOUF()
-        #assign_model(W_avg)
-        mon_sess.run(updated_paras, feed_dict={W_avg: (W_general.astype(np.float64))})
+        w=W_general.astype(np.float64)
+        mon_sess.run(updated_paras, feed_dict={W_avg: w})
         
         outer_i += 1
 
