8d51afe165d1f5488ce43bc49bdfae783fc61c10
Lynn1207 <linlintoo2@gmail.com>
2021-02-21 16:25:22 -0500

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -78,7 +78,7 @@ def barrier_update():
 class MyTCPHandler(socketserver.BaseRequestHandler):
 	def handle(self):
 		out_i=0
-		outer_iter=10
+		outer_iter=600
 		while out_i<outer_iter:
 			try:
 				#receive the size of content
