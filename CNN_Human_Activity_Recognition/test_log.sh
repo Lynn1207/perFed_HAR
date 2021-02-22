415e92de7abd83fd6239a96a447c8c369b4f5ba8
Lynn1207 <linlintoo2@gmail.com>
2021-02-21 23:27:37 -0500

    Update test_log.sh

--- CNN_Human_Activity_Recognition/test_log.sh
+++ CNN_Human_Activity_Recognition/test_log.sh
@@ -1,6 +1,6 @@
-cat log_test1.txt &
-cat log_test2.txt &
-cat log_test3.txt &
-cat log_test4.txt &
-cat log_test5.txt &
-cat log_test6.txt 
+cat log_test_FedAvg1.txt &
+cat log_test_FedAvg2.txt &
+cat log_test_FedAvg3.txt &
+cat log_test_FedAvg4.txt &
+cat log_test_FedAvg5.txt &
+cat log_test_FedAvg6.txt 
