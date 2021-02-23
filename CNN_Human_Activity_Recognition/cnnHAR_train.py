6c4c55b477abe919b3366f7a599cad20aab38410
Lynn1207 <linlintoo2@gmail.com>
2021-02-22 23:56:24 -0500

    Add files via upload

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -202,7 +202,7 @@ def train():
           step+=1
           
         outer_i += 1
-        
+        '''
         #get the weights and send to server
         w_flat = np.array([])
         for i in range(len(all_paras)):
@@ -215,7 +215,7 @@ def train():
         W_general = comm.recvOUF()
         #w = tf.cast(W_general, tf.float64)
         updated_paras_v=mon_sess.run(updated_paras, feed_dict={W_avg: W_general.astype(np.float64)})
-	
+	'''
         
         
 
