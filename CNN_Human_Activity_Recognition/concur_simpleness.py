4d76e6dfd775aed925231df9331fd43693a8a519
Lynn1207 <linlintoo2@gmail.com>
2021-02-23 17:31:28 -0500

    Add files via upload

--- CNN_Human_Activity_Recognition/concur_simpleness.py
+++ CNN_Human_Activity_Recognition/concur_simpleness.py
@@ -22,10 +22,24 @@ for i in range(1,7):
 
 #compute the concur_simpleness matrix
 concur_m=np.zeros((6,6))
+sum_matr=0.0
+count=0
 for i in range(6):
     for j in range(i+1, 6):
         concur_m[i][j]=concur_simp(accs[i], accs[j])
+        count+=1
+        sum_matr+=concur_m[i][j]
 
 print(concur_m)
-        
-        
+
+#grouping using concur_simp
+avg_consim=sum_matr/count
+print("\n Avarage concurrent_simpleness: %.3f. \n"%avg_consim)
+for i in range(6):
+    for j in range(i+1, 6):
+        if concur_m[i][j]>avg_consim:
+            concur_m[i][j]=1
+        else:
+            concur_m[i][j]=0
+
+print(concur_m)
