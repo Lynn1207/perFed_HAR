e2a60ab628a77e85c09886ae55a18b7f82f74846
Lynn1207 <linlintoo2@gmail.com>
2021-02-23 23:45:50 -0500

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -14,7 +14,7 @@ tf.disable_v2_behavior()
 
 NUM_OF_TOTAL_USERS = 6
 NUM_OF_WAIT = NUM_OF_TOTAL_USERS
-W_DIM = 8288 #l1: 2112; l2: 8288; l3: 599648; l6: 615224
+W_DIM = 74848 #l1: 2112; l2: 8288; l3: 74848, l4: 599648; l6: 615224
 inner_iteration = 5
 T_thresh = 10
 
@@ -34,18 +34,17 @@ update_flag = np.ones(NUM_OF_TOTAL_USERS)
 
 def server_update():
     
-    global W_avg1_1,W_avg2_1#,W_avg2_2, W_avg3_1, W_avg3_2, W_avg3_3, W_avg4_1, W_avg4_2, W_avg4_3, W_avg4_4, W_avg5_1, W_avg5_2, W_avg5_3, W_avg5_4, W_avg6_1, W_avg6_2, W_avg6_3, W_avg6_4
+    global W_avg1_1,W_avg2_1,W_avg3_1, W_avg3_2#, W_avg3_3, W_avg4_1, W_avg4_2, W_avg4_3, W_avg4_4, W_avg5_1, W_avg5_2, W_avg5_3, W_avg5_4, W_avg6_1, W_avg6_2, W_avg6_3, W_avg6_4
     # print(np.max(W))
     
     
     W_avg1_1= np.mean(W[0:6,0:2112], axis = 0)
     
     W_avg2_1=np.mean(W[0:6, 2112:8288], axis = 0)
-    '''
-    W_avg2_2=np.mean(W[5:6, 2112:8288], axis = 0)
     
-    W_avg3_1=(np.array(W[4][8288:74848])+np.array(W[0][8288:74848]))/2.0
+    W_avg3_1=(np.array(W[0][8288:74848])+np.array(W[4][8288:74848]+np.array(W[5][8288:74848])))/3.0
     W_avg3_2=np.mean(W[1:4, 8288:74848], axis = 0)
+    '''
     W_avg3_3=W[5][8288:74848]
     
     W_avg4_1=W[0][74848:599648]
@@ -158,8 +157,10 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     except Exception as e:
                         print("wait W timeout...")
                         
-                    if user_id[0]<=6 and user_id[0]>=1:
-                        W_avg=np.concatenate((W_avg1_1, W_avg2_1))#, W_avg3_2, W_avg4_2,W_avg5_2,W_avg6_2))
+                    if user_id[0]<=4 and user_id[0]>=2:
+                        W_avg=np.concatenate((W_avg1_1, W_avg2_1,W_avg3_2))
+                    else:
+                        W_avg=np.concatenate((W_avg1_1, W_avg2_1,W_avg3_1))
                     '''
                     elif user_id[0]==1:
                         np.concatenate((W_avg1_1, W_avg2_1, W_avg3_1, W_avg4_1,W_avg5_1,W_avg6_1))
