5aca559e9338dbeb56fe66cb3ff1fdb7ff15b6c1
Lynn1207 <linlintoo2@gmail.com>
2021-02-23 23:55:40 -0500

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -16,9 +16,9 @@ import numpy as np
 import cnnHAR_input
 
 #2 baselines, our method: fedper
-method="Local3" #"local", "FedPer"
-cur_l=3
-num_paras=74848
+method="Local4" #"local", "FedPer"
+cur_l=4
+num_paras=599648
 
 # Basic model parameters.
 batch_size = 32
