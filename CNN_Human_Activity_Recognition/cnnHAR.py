96348316b76337c9de0afbc82b2740d19ca46b66
Lynn1207 <linlintoo2@gmail.com>
2021-02-24 19:27:49 -0500

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -278,7 +278,7 @@ def train(total_loss, global_step):#index is a string e.g. '_1'
 
  # Compute gradients.
  with tf.control_dependencies([loss_averages_op]):
-  opt = tf.train.AdamOptimizer(lr)#opt = tf.train.AdadeltaOptimizer(lr)
+  opt = tf.train.MomentumOptimizer(lr,0.9)#opt = tf.train.AdadeltaOptimizer(lr)
   grads = opt.compute_gradients(total_loss)
   #for i in range(0,len(grads)):
     #print(i)
