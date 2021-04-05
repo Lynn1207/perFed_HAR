356c25e93d253b5ba0a75774e1be05945a805008
Lynn1207 <linlintoo2@gmail.com>
2021-04-05 00:02:33 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -153,7 +153,7 @@ def inference(signals):
    
     with tf.variable_scope('conv1') as scope:
            kernel = _variable_with_weight_decay('weights1',
-                                                shape=[ 32, 3, 1, 64],
+                                                shape=[ 32, 3, 2, 64],
                                                 #shape=[3, 1, 128],
                                                 stddev=0.04,
                                                 wd=0.001)
@@ -169,12 +169,12 @@ def inference(signals):
    
     with tf.variable_scope('conv2') as scope:
            kernel = _variable_with_weight_decay('weights2',
-                                                shape=[ 3, 2, 64, 32],
+                                                shape=[ 3, 1, 64, 32],
                                                 #shape=[3, 1, 128],
                                                 stddev=0.04,
                                                 wd=0.001)
            biases = _variable_on_cpu('biases2', [32], tf.constant_initializer(0.0))#!!!
-           conv = tf.nn.conv2d(pool1, kernel, [1,1,2,1], padding='VALID', data_format='NHWC')
+           conv = tf.nn.conv2d(pool1, kernel, [1,1,1,1], padding='VALID', data_format='NHWC')
            pre_activation = tf.nn.bias_add(conv, biases)
            conv2 = tf.nn.relu(pre_activation, name=scope.name)
            _activation_summary(conv2)
