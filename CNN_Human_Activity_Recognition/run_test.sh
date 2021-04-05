d2927dcb493646ee5ccf5c1e64a85f478c5a1a99
Lynn1207 <linlintoo2@gmail.com>
2021-04-05 16:01:56 -0400

    Delete CNN_Human_Activity_Recognition directory

--- CNN_Human_Activity_Recognition/run_test.sh
+++
@@ -1,6 +0,0 @@
-python ./cnnHAR_train.py 0 &
-python3 ./cnnHAR_train.py 1 &
-python3 ./cnnHAR_train.py 2 &
-python3 ./cnnHAR_train.py 3 &
-python3 ./cnnHAR_train.py 4 &
-python3 ./cnnHAR_train.py 5 &
--- CNN_Human_Activity_Recognition/test_log.sh
+++
@@ -1,6 +0,0 @@
-cat log_test_FedAvg1.txt &
-cat log_test_FedAvg2.txt &
-cat log_test_FedAvg3.txt &
-cat log_test_FedAvg4.txt &
-cat log_test_FedAvg5.txt &
-cat log_test_FedAvg6.txt 
