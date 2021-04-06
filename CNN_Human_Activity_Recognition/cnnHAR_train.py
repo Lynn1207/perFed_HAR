7908c2542ef1f6a9dd7beb4cd5865105cfde78e5
Lynn1207 <linlintoo2@gmail.com>
2021-04-06 14:01:08 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -216,8 +216,8 @@ def train():
         for i in range(cur_layer*2):
           temp = all_paras[i].reshape(-1)
           w_flat=np.concatenate((w_flat, temp), axis=0)
-        #if str(sys.argv[1])=="1":
-          #print("Before_merge:", w_flat[0:3])
+        if str(sys.argv[1])=="1":
+          print("Before_merge:", w_flat[0:3])
         comm.send2server(w_flat,0)
       
         #receive aggregated weights from server
@@ -225,9 +225,9 @@ def train():
         #w = tf.cast(W_general, tf.float64)
         if not mon_sess.should_stop():
           updated_paras_v=mon_sess.run(updated_paras, feed_dict={W_avg: W_general.astype(np.float64)})
-          #print("W_avg:", W_general[0:3])
-          #if str(sys.argv[1])=="1":
-            #print("After_merge:", updated_paras_v[0].reshape(-1)[0:3])
+          if str(sys.argv[1])=="1":
+            print("W_avg:", W_general[0:3])
+            print("After_merge:", updated_paras_v[0].reshape(-1)[0:3])
         #print("Length of updated paras: %d \n"% len(updated_paras_v))
         
   
