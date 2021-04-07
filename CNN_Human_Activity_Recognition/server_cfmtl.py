992b8aeded73ec12b78352011fa25ab5abc4f6dc
Lynn1207 <linlintoo2@gmail.com>
2021-04-07 18:13:51 -0400

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -48,8 +48,8 @@ def server_update():
     W_avg3_1=(np.array(W[0][52896:163872])+np.array(W[2][52896:163872]))/2.0
     W_avg3_2=(np.array(W[1][52896:163872])+np.array(W[3][52896:163872])+np.array(W[4][52896:163872])+np.array(W[5][52896:163872]))/4.0
    
-    W_avg4_1=W[0:1, 163872:213152]
-    W_avg4_2=W[2:3, 163872:213152]
+    W_avg4_1=W[0][163872:213152]
+    W_avg4_2=W[2][163872:213152]
     W_avg4_3=(np.array(W[1][163872:213152])+np.array(W[3][163872:213152])+np.array(W[4][163872:213152])+np.array(W[5][163872:213152]))/4.0
     '''
     W_avg5_1=W[0:1, 773728:789118]
@@ -161,6 +161,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         W_avg=np.concatenate((W_avg1_1, W_avg2_1,W_avg3_1,W_avg4_2))#, W_avg2_1,W_avg3_2,W_avg4_2, W_avg5_2, W_avg6_2))
                     else: 
                         W_avg=np.concatenate((W_avg1_1, W_avg2_1,W_avg3_2,W_avg4_3))#, W_avg2_1,W_avg3_1,W_avg4_1, W_avg5_3, W_avg6_3))
+                    print(out_i, user_id[0])
                                              
                 
                     W_avg_data = pickle.dumps(W_avg, protocol = 0)
