9dea3e96195cb5acf09e94c3e28e3785a20bc727
Lynn1207 <linlintoo2@gmail.com>
2021-04-07 16:00:24 -0400

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -41,7 +41,7 @@ def server_update():
     #W_avg=np.mean(W, axis=0)
    
     
-    W_avg1_1= np.mean(W[:,0:1664], axis = 0)
+    W_avg1_1= np.mean(W[0:6,0:1664], axis = 0)
     '''
     W_avg2_1=W[0:1, 12352:18528]
     W_avg2_2=np.mean(W[1:6, 12352:18528], axis = 0)
