b1086d93ef3a42c14c1b318833352d2284dd8b06
Lynn1207 <linlintoo2@gmail.com>
2021-04-18 20:22:05 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -292,10 +292,10 @@ def train(total_loss, global_step):#index is a string e.g. '_1'
   ###### Record the parameters
   paras=[]
   for var in tf.trainable_variables():
-    if var.op.name.find('batch_normalization') != -1:
+    if var.op.name.find('batch_normalization') == -1:
       paras.append(var)
     #if str(sys.argv[1])=="1":
-      print("Before_merge:", var.op.name)
+      #print("Before_merge:", var.op.name)
     
  return  variables_averages_op, paras
 
