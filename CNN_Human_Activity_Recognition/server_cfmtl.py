7163b0f07569f9ff14540e94e8778a08b820b3c4
Lynn1207 <linlintoo2@gmail.com>
2021-04-18 22:58:31 -0400

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -14,7 +14,7 @@ tf.disable_v2_behavior()
 
 NUM_OF_TOTAL_USERS = 6
 NUM_OF_WAIT = NUM_OF_TOTAL_USERS
-W_DIM =14432#l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5:776806
+W_DIM =125408#l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5:776806
 inner_iteration = 5
 T_thresh = 10
 
@@ -43,10 +43,10 @@ def server_update():
     
     W_avg2_1=(W[0][6208:14432]+W[1][6208:14432]+W[4][6208:14432]+W[3][6208:14432]+W[5][6208:14432])/5.0
     W_avg2_2=W[2][6208:14432]
-    '''
-    W_avg3_1=(W[0][14432:125408]+W[1][14432:125408]+W[2][14432:125408]+W[3][14432:125408]+W[5][14432:125408])/5.0
-    W_avg3_2=W[4][14432:125408]
     
+    W_avg3_1=(W[0][14432:125408]+W[1][14432:125408]+W[4][14432:125408]+W[3][14432:125408]+W[5][14432:125408])/5.0
+    W_avg3_2=W[2][14432:125408]
+    '''
     W_avg4_1=(W[0][125408:199328]+W[1][125408:199328]+W[2][125408:199328]+W[3][125408:199328]+W[5][125408:199328])/5.0
     W_avg4_2=W[4][125408:199328]
     
@@ -169,9 +169,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         
                     
                     if user_id[0]==3 :
-                        W_avg=np.concatenate((W_avg1_2, W_avg2_2))
+                        W_avg=np.concatenate((W_avg1_2, W_avg2_2,W_avg3_2))
                     else: 
-                        W_avg=np.concatenate((W_avg1_1, W_avg2_1))#, W_avg2_1,W_avg3_1,W_avg4_1, W_avg5_3, W_avg6_3))
+                        W_avg=np.concatenate((W_avg1_1, W_avg2_1,W_avg3_1))#, W_avg2_1,W_avg3_1,W_avg4_1, W_avg5_3, W_avg6_3))
                     #print(out_i, user_id[0])
                                       
                 
