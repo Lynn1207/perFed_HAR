44a258ff9f265a0b657b696fab275f6123ba0b61
Lynn1207 <linlintoo2@gmail.com>
2021-04-20 17:28:56 -0400

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -14,7 +14,7 @@ tf.disable_v2_behavior()
 
 NUM_OF_TOTAL_USERS = 8
 NUM_OF_WAIT = NUM_OF_TOTAL_USERS
-W_DIM =100832#l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5:776806
+W_DIM =174752#l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5:776806
 inner_iteration = 5
 T_thresh = 10
 
@@ -36,18 +36,17 @@ def server_update():
     
     global W_avg, W_avg1_1,W_avg1_2, W_avg2_1,W_avg2_2,W_avg2_3,W_avg2_4,W_avg3_1, W_avg3_2, W_avg3_3,W_avg3_4,W_avg3_5,W_avg3_6, W_avg4_1, W_avg4_2,W_avg4_3, W_avg4_4,W_avg4_5, W_avg4_6,W_avg5_1, W_avg5_2, W_avg5_3, W_avg5_4,W_avg5_5, W_avg5_6
     # print(np.max(W))
-    W_avg=np.mean(W, axis = 0)
-    '''
-    W_avg1_1=np.mean(W[0:24, 0:6208], axis = 0)
+    #W_avg=np.mean(W, axis = 0)
     
-    W_avg2_1=np.mean(W[0:24, 6208:14432], axis = 0)
+    W_avg1_1=np.mean(W[:, 0:2112], axis = 0)
+    
+    W_avg2_1=np.mean(W[:, 2112:14432], axis = 0)
    
-    W_avg3_1=np.mean(np.concatenate((W[0:5,14432:125408], W[6:24,14432:125408])), axis = 0)
-    W_avg3_2=W[5, 14432:125408]
+    W_avg3_1=np.mean(W[:,14432:100832], axis = 0)
     
-    W_avg4_1=np.mean(np.concatenate((W[0:5,125408:199328], W[6:24,125408:199328])), axis = 0)
-    W_avg4_2=W[5, 125408:199328]
-    
+    W_avg4_1=W[0, 100832:174752]
+    W_avg4_2=np.mean(W[1:8,100832:174752], axis = 0)
+    '''
     W_avg5_1=np.mean(np.concatenate((W[0:5,199328:200293], W[6:24,199328:200293])), axis = 0)
     W_avg5_2=W[5, 199328:200293]
     '''
@@ -144,12 +143,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     except Exception as e:
                         print("wait W timeout...")
                         
-                    '''
-                    if user_id[0]==6:
-                        W_avg=np.concatenate((W_avg1_1, W_avg2_1, W_avg3_2, W_avg4_2, W_avg5_2))
+                    
+                    if user_id[0]==1:
+                        W_avg=np.concatenate((W_avg1_1, W_avg2_1, W_avg3_1, W_avg4_1))
                     else:
-                        W_avg=np.concatenate((W_avg1_1, W_avg2_1, W_avg3_1, W_avg4_1, W_avg5_1))
-                    '''
+                        W_avg=np.concatenate((W_avg1_1, W_avg2_1, W_avg3_1, W_avg4_2))
+                    
                                    
                 
                     W_avg_data = pickle.dumps(W_avg, protocol = 0)
