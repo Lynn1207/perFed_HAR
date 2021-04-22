23471298cb909b065ce42ea748d9e545297bef35
Lynn1207 <linlintoo2@gmail.com>
2021-04-22 15:43:53 -0400

    Update cnnHAR_input.py

--- CNN_Human_Activity_Recognition/cnnHAR_input.py
+++ CNN_Human_Activity_Recognition/cnnHAR_input.py
@@ -36,7 +36,7 @@ channels=1
 batch_per_user_train=7
 batch_per_user_test=2
 # Global constants describing the cnnHAR data set.
-NUM_CLASSES = 2
+NUM_CLASSES = 5
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 32*batch_per_user_train
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 32*batch_per_user_test
 
