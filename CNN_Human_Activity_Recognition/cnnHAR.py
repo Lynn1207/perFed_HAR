41cf6b97f1fd32c0ea2dc3123164ffc51f490afc
Lynn1207 <linlintoo2@gmail.com>
2021-04-22 16:35:46 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -23,7 +23,7 @@ num_paras=174752#l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5: 213797
 # Basic model parameters.
 batch_size = 32
                           
-data_dir = '/home/ubuntu/perFed_HAR/CNN_Human_Activity_Recognition/dataset/'
+data_dir = '/home/ubuntu/perFed_HAR/CNN_Human_Activity_Recognition/images/'
                     
 
 # Global constants describing the CIFAR-10 data set.
@@ -153,34 +153,34 @@ def inference(signals):
     #print('<<<<<<<<<<<<<<<<<<<<Shape of singals :',signals.get_shape())
     with tf.variable_scope('conv1') as scope:
            kernel = _variable_with_weight_decay('weights1',
-                                                shape=[32,1, 64],
+                                                shape=[5,5, 1, 64],
                                                 #shape=[3, 1, 128],
                                                 stddev=0.04,
                                                 wd=0.009)
            biases = _variable_on_cpu('biases1', [64], tf.constant_initializer(0.0))#!!!
-           conv = tf.nn.conv1d(signals, kernel, [1,1,1], padding='VALID', data_format='NWC')
+           conv = tf.nn.conv2d(signals, kernel, [1,1,1,1], padding='SAME', data_format='NHWC')
            pre_activation = tf.nn.bias_add(conv, biases)#tf.layers.batch_normalization(tf.nn.bias_add(conv, biases))
            #pre_activation= tf.layers.batch_normalization(tf.nn.bias_add(conv, biases))
            conv1 = tf.nn.relu(pre_activation, name=scope.name)
            #_activation_summary(conv1)
            #print ('<<<<<<<<<<<<<<<<<<<<Shape of conv1 :',conv1.get_shape())
-    pool1 = tf.nn.max_pool1d(conv1, ksize=[1,1,1], strides=[1,1,1],padding='VALID',name='pool1')
+    pool1 = tf.nn.max_pool2d(conv1, ksize=[1,2,2,1], strides=[1,2,2,1],padding='SAME',name='pool1')
     #print ('<<<<<<<<<<<<<<<<<<<<Shape of pool1 :',pool1.get_shape())
-    """18x3x64"""
+    """18x18x64"""
    
     with tf.variable_scope('conv2') as scope:
            kernel = _variable_with_weight_decay('weights2',
-                                                shape=[ 6, 64, 32],
+                                                shape=[6,6, 64, 32],
                                                 #shape=[3, 1, 128],
                                                 stddev=0.04,
                                                 wd=0.009)
            biases = _variable_on_cpu('biases2', [32], tf.constant_initializer(0.0))#!!!
-           conv = tf.nn.conv1d(pool1, kernel, [1,3,1], padding='VALID', data_format='NWC')
+           conv = tf.nn.conv2d(pool1, kernel, [1,2,2,1], padding='SAME', data_format='NHWC')
            pre_activation=tf.nn.bias_add(conv, biases)#tf.layers.batch_normalization(tf.nn.bias_add(conv, biases))
            conv2 = tf.nn.relu(pre_activation, name=scope.name)
            #_activation_summary(conv2)
            #print ('<<<<<<<<<<<<<<<<<<<<Shape of conv2:',conv2.get_shape()) 
-    pool2 = tf.nn.max_pool1d(conv2, ksize=[1,1,1], strides=[1,1,1],padding='VALID',name='pool2')
+    pool2 = tf.nn.max_pool2d(conv2, ksize=[1,3,3,1], strides=[1,2,2,1],padding='SAME',name='pool2')
     #print ('<<<<<<<<<<<<<<<<<<<<Shape of pool2 :',pool2.get_shape()) 
     reshape = tf.keras.layers.Flatten()(pool2)
     
@@ -340,7 +340,7 @@ def reset_var(W_avg):
   for var in tf.trainable_variables():
     if cur_l>0:
       if var.op.name=="conv1/weights1":
-        var=tf.assign(var, tf.reshape(W_avg[0:2048],[32,  1, 64]))
+        var=tf.assign(var, tf.reshape(W_avg[0:2048],[5,5,1, 64]))
         updated_paras.append(var)
       elif var.op.name=="conv1/biases1":
         var=tf.assign(var, tf.reshape(W_avg[2048:2112],[64,]))
