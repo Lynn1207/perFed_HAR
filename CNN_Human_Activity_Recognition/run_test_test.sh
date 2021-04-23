116bc8fd3bdd763bd20980e2b9dedc64a5a629b8
Lynn1207 <linlintoo2@gmail.com>
2021-04-23 03:24:38 -0400

    Update run_test_test.sh

--- CNN_Human_Activity_Recognition/run_test_test.sh
+++ CNN_Human_Activity_Recognition/run_test_test.sh
@@ -1,1 +1,8 @@
-taskset --cpu-list 0 python ./cnnHAR_train.py 8
+taskset --cpu-list 0 python ./cnnHAR_train.py 1 &
+taskset --cpu-list 1 python ./cnnHAR_train.py 2 &
+taskset --cpu-list 2 python ./cnnHAR_train.py 3 &
+taskset --cpu-list 3 python ./cnnHAR_train.py 4 &
+taskset --cpu-list 4 python ./cnnHAR_train.py 5 &
+taskset --cpu-list 5 python ./cnnHAR_train.py 6 &
+taskset --cpu-list 6 python ./cnnHAR_train.py 7 &
+taskset --cpu-list 7 python ./cnnHAR_train.py 8 
