80eac135ce377f0ff6ba8e8d65a5a5c806343c98
Lynn1207 <linlintoo2@gmail.com>
2021-04-23 04:45:37 -0400

    Update cnnHAR_input.py

--- CNN_Human_Activity_Recognition/cnnHAR_input.py
+++ CNN_Human_Activity_Recognition/cnnHAR_input.py
@@ -57,8 +57,12 @@ def read_cnnHAR(filename_queue):
   
   record_bytes = tf.decode_csv(value, record_defaults = record_defaults)
   # The first bytes represent the label, which we convert from uint8->int32.
-  result.signal = tf.cast(
-      tf.strided_slice(record_bytes, [1], [SIGNAL_SIZE*axis*channels+1])/255, tf.float32)
+  if int(sys.argv[1])<4
+    result.signal = tf.cast(tf.strided_slice(record_bytes, [1], [SIGNAL_SIZE*axis*channels+1])/255*0.6, tf.float32)
+  elif int(sys.argv[1])<7
+    result.signal = tf.cast(tf.strided_slice(record_bytes, [1], [SIGNAL_SIZE*axis*channels+1])/255*0.35, tf.float32)
+  else:
+    result.signal = tf.cast(tf.strided_slice(record_bytes, [1], [SIGNAL_SIZE*axis*channels+1])/255, tf.float32)
   #print('!!!!!!!!!!!!!!!!!!! result.signals', result.signal.get_shape())
   result.signal = tf.reshape(result.signal, [channels, axis, SIGNAL_SIZE])
   #print('!!!!!!!!!!!!!!!!!!! result.signals', result.signal.get_shape())
