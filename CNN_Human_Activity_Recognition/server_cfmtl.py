3f46607d133ad5aba159e77760a31895914e0c16
Lynn1207 <linlintoo2@gmail.com>
2021-04-30 02:06:37 -0400

    Update server_cfmtl.py

--- CNN_Human_Activity_Recognition/server_cfmtl.py
+++ CNN_Human_Activity_Recognition/server_cfmtl.py
@@ -49,7 +49,7 @@ def server_update():
         print(group, sum(group.values()))
         tmp_w=np.zeros(1,1664)
         for key in group:
-            print(key, W[key-1, 0:3])
+            print(key)
         W_l1.append(tmp_w)
     '''
         tmp_w=np.zeros(1,1664)
