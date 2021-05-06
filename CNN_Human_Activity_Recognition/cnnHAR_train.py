b1d7a2939741232d1f46bd1c129420b402c11675
Lynn1207 <linlintoo2@gmail.com>
2021-05-06 18:50:09 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -1,38 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""A binary to train CIFAR-10 using a single GPU.
-
-Accuracy:
-cifar10_train.py achieves ~86% accuracy after 100K steps (256 epochs of
-data) as judged by cifar10_eval.py.
-
-Speed: With batch_size 128.
-
-System        | Step Time (sec/batch)  |     Accuracy
-------------------------------------------------------------------
-1 Tesla K20m  | 0.35-0.60              | ~86% at 60K steps  (5 hours)
-1 Tesla K40m  | 0.25-0.35              | ~86% at 100K steps (4 hours)
-
-Usage:
-Please see the tutorial and website for how to download the CIFAR-10
-data set, compile the program and train the model.
-
-http://tensorflow.org/tutorials/deep_cnn/
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -78,15 +43,12 @@ def train():
   #gen_paras=[]
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
   tf.get_logger().setLevel("ERROR")
-  """Train CIFAR-10 for a number of steps."""
+  """Train CNNHAR for a number of steps."""
   with tf.Graph().as_default():
     
     global_step = tf.train.get_or_create_global_step()
-    # Get images and labels for CIFAR-10.
-    # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
-    # GPU and resulting in a slow down.
-    #with tf.device('/cpu:'+str(int(sys.argv[1])-1)):
-    #print('<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>')
+    # Get images and labels for CNNHAR.
+    # Force input pipeline to CPU:i to simulate federated setting
       
     # Build a Graph that computes the logits predictions from the
     # inference model.
@@ -261,8 +223,7 @@ def train():
 
       
 
-def main(argv=None):  # pylint: disable=unused-argument
-#  cifar10.maybe_download_and_extract()
+def main(argv=None):  
   if tf.gfile.Exists(train_dir):
     tf.gfile.DeleteRecursively(train_dir)
   tf.gfile.MakeDirs(train_dir)
