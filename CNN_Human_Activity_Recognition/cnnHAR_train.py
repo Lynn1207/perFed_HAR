50956190d695d5443f399354de146589542a27e2
Lynn1207 <linlintoo2@gmail.com>
2021-05-17 21:47:24 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -151,6 +151,8 @@ def train():
 
     outer_i = 0
     start_iter=8
+    cur_layer=0
+    intvl=0
     with tf.train.MonitoredTrainingSession(
           checkpoint_dir=train_dir,
           hooks=[tf.train.StopAtStepHook(last_step=max_steps*outer_iter+1),
@@ -166,16 +168,20 @@ def train():
           _, all_paras,_=mon_sess.run([train_op,paras, extra_update_ops])
           step+=1
           
+           
         outer_i += 1
         if outer_i>=start_iter:
           #get the weights and send to server
           w_flat = np.array([])
           #depends on how many layer wanna upload to server to share with other users
           #six layers: 2,4,6,8,10,11, or len(all_paras).
-          cur_layer=int(outer_i/start_iter)#cnnHAR.cur_l
-          if outer_i%start_iter==0:
+          intvl+=1
+          if start_iter==intvl:
+            cur_layer=min(cur_layer+1,3)
             print(cur_layer, start_iter)
             start_iter=int(start_iter*0.8)
+            intvl=0
+          
           for i in range(cur_layer*2):
             temp = all_paras[i].reshape(-1)
             w_flat=np.concatenate((w_flat, temp), axis=0)
