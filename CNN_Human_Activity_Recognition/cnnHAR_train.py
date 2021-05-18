ceb70c6837d401a4bf396f335cfb5b97a4d632c9
Lynn1207 <linlintoo2@gmail.com>
2021-05-18 01:26:47 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -175,10 +175,10 @@ def train():
           w_flat = np.array([])
           #depends on how many layer wanna upload to server to share with other users
           #six layers: 2,4,6,8,10,11, or len(all_paras).
-          if start_iter==intvl:
+          if cur_layer<3 and start_iter==intvl:
             cur_layer=min(cur_layer+1,3)
             #print(cur_layer, start_iter)
-            start_iter=int(start_iter*1)
+            start_iter=int(start_iter*0.8)
             intvl=0
           
           for i in range(cur_layer*2):
