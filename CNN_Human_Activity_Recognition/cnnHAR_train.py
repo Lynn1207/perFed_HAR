920fa183cb743e1de10bfefac3a073468becc1f2
Lynn1207 <linlintoo2@gmail.com>
2021-05-18 20:26:01 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -153,7 +153,7 @@ def train():
 
     outer_i = 0
     start_iter=6
-    cur_layer=4
+    cur_layer=0
     intvl=0
     with tf.train.MonitoredTrainingSession(
           checkpoint_dir=train_dir,
@@ -173,19 +173,18 @@ def train():
         outer_i += 1
         
         intvl+=1 
-        
-        if outer_i>=0:#start_iter:
+        '''
+        if outer_i>=start_iter:
           #get the weights and send to server
           w_flat = np.array([])
           #depends on how many layer wanna upload to server to share with other users
           #six layers: 2,4,6,8,10,11, or len(all_paras).
-          '''
+         
           if cur_layer<3 and start_iter==intvl:
             cur_layer=min(cur_layer+1,3)
             #print(cur_layer, start_iter)
             start_iter=int(start_iter*0.5)
             intvl=0
-          '''  
           
           for i in range(cur_layer*2):
             temp = all_paras[i].reshape(-1)
@@ -217,7 +216,7 @@ def train():
               #print("W_avg:", W_general[0:3])
               #print("After_merge:", updated_paras_v[0].reshape(-1)[0:3])
           #print("Length of updated paras: %d \n"% len(updated_paras_v))
-          
+          '''
           
         
         
