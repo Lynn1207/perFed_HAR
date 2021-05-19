410346c735ca7bfe747a623ca0d74fff240ce555
Lynn1207 <linlintoo2@gmail.com>
2021-05-18 21:17:53 -0400

    Update cnnHAR_train.py

--- CNN_Human_Activity_Recognition/cnnHAR_train.py
+++ CNN_Human_Activity_Recognition/cnnHAR_train.py
@@ -153,7 +153,7 @@ def train():
 
     outer_i = 0
     start_iter=4 #6:20
-    cur_layer=3
+    cur_layer=4
     intvl=0
     with tf.train.MonitoredTrainingSession(
           checkpoint_dir=train_dir,
