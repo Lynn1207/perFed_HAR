556c520c8150af539a50f689fbe1026419e45e6f
Lynn1207 <linlintoo2@gmail.com>
2021-05-19 15:18:06 -0400

    Update cnnHAR.py

--- CNN_Human_Activity_Recognition/cnnHAR.py
+++ CNN_Human_Activity_Recognition/cnnHAR.py
@@ -16,7 +16,7 @@ import numpy as np
 import cnnHAR_input
 
 #2 baselines, our method: fedper
-method="FedAvg" #"local", "FedPer"
+method="FedAvgLK" #"local", "FedPer"
 #cur_l=4
 num_paras=175138 #l1: 1664; l2: 52896; l3: 163872, l4: 213152; l5: 213797
 
